989a05ffb97f8c3ede91fcc48bd73cbd3951f8fc
Dave Burgess <contact@daveburgess.uk>
2019-09-18 22:59:44 +0100

    Adds prime number divisible by message

--- src/Number.php
+++ src/Number.php
@@ -6,6 +6,8 @@ class Number
     /** @var int */
     private $number;
 
+    public $message;
+
     public function __construct(int $number)
     {
         $this->number = $number;
@@ -13,17 +15,20 @@ class Number
 
     public function isPrime(): bool
     {
-        if($this->number == 1){
+        if($this->number <= 1){
+            $this->message = 'number is lower than 2';
             return false;
         }
 
         if($this->number % 2 === 0 && $this->number !== 2){
+            $this->message = 'divisible by ' . 2;
             return false;
         }
 
         if($this->number > 3){
             for($n = 3; $n < $this->number; $n = $n+2){
                 if($this->number % $n == 0 && $this->number != $n){
+                    $this->message = 'divisible by ' . $n;
                     return false;
                 }
             }
--- tests/NumberTest.php
+++ tests/NumberTest.php
@@ -19,6 +19,8 @@ class PrimeNumberRecognitionTest extends TestCase
     public function nonePrimeNumbers()
     {
         return [
+            "-1" => [-1],
+            "0" => [0],
             "1" => [1],
             "4" => [4],
             "6" => [6],
@@ -29,6 +31,7 @@ class PrimeNumberRecognitionTest extends TestCase
             "21" => [21],
             "25" => [25],
             "33" => [33],
+            "100" => [100],
         ]; 
     }
 
@@ -57,6 +60,49 @@ class PrimeNumberRecognitionTest extends TestCase
             "23" => [23],
             "29" => [29],
             "31" => [31],
+            "37" => [37],
+            "41" => [41],
+            "43" => [43],
+            "47" => [47],
+            "53" => [53],
+            "59" => [59],
+            "61" => [61],
+            "67" => [67],
+            "71" => [71],
+            "73" => [73],
+            "79" => [79],
+            "83" => [83],
+            "89" => [89],
+            "97" => [97],
+            "617" => [617],
         ];
+    }
+
+    /**
+     * @test
+     */
+    public function itCanStoreAMessageWhenFalse()
+    {
+        $number = new Number(4);
+        $number->isPrime();
+        $this->assertEquals($number->message, 'divisible by 2');
+
+        $number = new Number(1);
+        $number->isPrime();
+        $this->assertEquals($number->message, 'number is lower than 2');
+    }
+
+    /**
+     * @test
+     */
+    public function outputALoadOfNumbersToSenseCheck()
+    {
+        $numbersToTest = 100;
+        $primeCount = 0;
+        for($n = -10; $n <= $numbersToTest; $n++){
+            $number = new Number($n);
+            echo "\r\nNumber $n: " . ($number->isPrime() ? 'true': 'false - ' . $number->message);
+        }
+        $this->assertEquals($n, $numbersToTest+1);
     }
 }
